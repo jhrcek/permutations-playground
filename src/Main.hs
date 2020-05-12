8a63ad5513bc3070225dcdd652592692b1a92f7a
Jan Hrček <jhrcek@globalwebindex.com>
2020-05-12 15:47:55 +0200

    Add more endpoints

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,9 @@ import qualified Data.Array as Array
 import qualified Data.ByteString.Builder as Builder
 import qualified Data.ByteString.Lazy as LBS
 import Data.Char (digitToInt)
+import qualified Data.Foldable as Fold
+import qualified Data.List as List
+import Data.Maybe (fromMaybe)
 import Data.Proxy
 import Math.Combinat.Permutations
 import Network.HTTP.Media
@@ -20,12 +23,21 @@ import qualified Network.Wai.Handler.Warp as Warp
 import Servant
 import Servant.API
 import System.Random
+import Text.Read (readMaybe)
 import Turtle
 import qualified Turtle.Bytes
 
 main :: IO ()
 main = do
-  putStrLn "Try http://localhost:8080/permutation/random/10"
+  putStrLn $ unlines $
+    "Endpoints"
+      : fmap
+        ("http://localhost:8080/" <>)
+        [ "permutation/random/10",
+          "permutation/2341",
+          "permutation/2341/2",
+          "permtree/3"
+        ]
   Warp.runSettings settings app
 
 settings :: Warp.Settings
@@ -41,30 +53,73 @@ type RandomPermAPI =
   "permutation" :> "random" :> Capture "n" Int :> Get '[SVG] LBS.ByteString
     :<|> "permutation" :> Capture "perm" Int :> Get '[SVG] LBS.ByteString
     :<|> "permutation" :> Capture "perm" Int :> Capture "exponent" Int :> Get '[SVG] LBS.ByteString
+    :<|> "permtree" :> Capture "n" Int :> Get '[SVG] LBS.ByteString
 
 server :: Server RandomPermAPI
 server =
   randomPermutationH
     :<|> permutationH
     :<|> permutationPowerH
+    :<|> permTreeH
   where
     randomPermutationH :: Int -> Handler LBS.ByteString
     randomPermutationH n = do
       gen <- liftIO newStdGen
       let perm = fst $ randomPermutation n gen
       servePermutation perm
+    -- TODO change Int to Permutation
     permutationH :: Int -> Handler LBS.ByteString
-    permutationH permAsInt = do
-      let digits = map digitToInt $ show permAsInt
-      case maybePermutation digits of
-        Just perm -> servePermutation perm
-        Nothing -> pure $ "Not a permutation"
+    permutationH permAsInt =
+      withPermutation permAsInt servePermutation
     permutationPowerH :: Int -> Int -> Handler LBS.ByteString
-    permutationPowerH permAsInt power = do
-      let digits = map digitToInt $ show permAsInt
-      case maybePermutation digits of
-        Just perm -> servePermutation $ multiplyMany $ replicate power perm
-        Nothing -> pure $ "Not a permutation"
+    permutationPowerH permAsInt power =
+      withPermutation permAsInt (servePermutation . multiplyMany . replicate power)
+    permTreeH :: Int -> Handler LBS.ByteString
+    permTreeH n
+      | 1 <= n && n <= 4 = do
+        let allPerms = concatMap show <$> List.permutations [1 .. n]
+            edges = List.nub . List.sort $ Fold.concatMap permStrToEdges allPerms
+        renderGraphFromEdges Dot edges
+      | otherwise = throwError $ err400 {errBody = "Out of range [1 .. 4]"}
+
+permStrToEdges :: String -> [(Int, Int)]
+permStrToEdges str =
+  zipWith (,) xs (tail xs)
+  where
+    xs = rd <$> List.inits str
+    rd = fromMaybe 0 . readMaybe
+
+withPermutation :: Int -> (Permutation -> Handler LBS.ByteString) -> Handler LBS.ByteString
+withPermutation permAsInt respond = do
+  let digits = map digitToInt $ show permAsInt
+  case maybePermutation digits of
+    Just perm -> respond perm
+    Nothing -> throwError $ err400 {errBody = "Not a permutation"}
+
+servePermutation :: Permutation -> Handler LBS.ByteString
+servePermutation =
+  renderGraphFromEdges Circo . Array.assocs . permutationArray
+
+renderGraphFromEdges :: GraphvizEngine -> [(Int, Int)] -> Handler LBS.ByteString
+renderGraphFromEdges engine edges = do
+  let dotLines = fmap renderEdge edges
+  liftIO $ LBS.writeFile "tmp.dot" $ LBS.intercalate ";" $ "digraph {node[shape=circle]" : dotLines <> ["}"]
+  (_, graphSVG) <- Turtle.Bytes.shellStrict (showEngine engine <> " tmp.dot -Tsvg") empty
+  pure $ LBS.fromStrict graphSVG
+
+renderEdge :: (Int, Int) -> LBS.ByteString
+renderEdge (i, j) =
+  Builder.toLazyByteString $
+    Builder.intDec i
+      <> Builder.char8 '-'
+      <> Builder.char8 '>'
+      <> Builder.intDec j
+
+data GraphvizEngine = Dot | Circo
+
+showEngine :: GraphvizEngine -> Text
+showEngine Dot = "dot"
+showEngine Circo = "circo"
 
 data SVG
 
@@ -73,20 +128,3 @@ instance Servant.API.Accept SVG where
 
 instance MimeRender SVG LBS.ByteString where
   mimeRender _ = id
-
-servePermutation :: Permutation -> Handler LBS.ByteString
-servePermutation perm = do
-  let dotLines =
-        fmap
-          ( \(i, j) ->
-              Builder.toLazyByteString $
-                Builder.intDec i
-                  <> Builder.char8 '-'
-                  <> Builder.char8 '>'
-                  <> Builder.intDec j
-          )
-          . Array.assocs
-          $ permutationArray perm
-  liftIO $ LBS.writeFile "tmp.dot" $ LBS.intercalate ";" $ "digraph {node[shape=circle]" : dotLines <> ["}"]
-  (_, graphSVG) <- Turtle.Bytes.shellStrict "circo tmp.dot -Tsvg" empty
-  pure $ LBS.fromStrict graphSVG
