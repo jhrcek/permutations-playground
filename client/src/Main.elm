bdc05908d1a4c298028a54901751fccff34f7f3b
Jan Hrcek <honza.hrk@gmail.com>
2021-06-13 10:52:56 +0200

    Remove unused stuff

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -2,15 +2,11 @@ module Main exposing (main)
 
 import Array exposing (Array)
 import Browser
-import Browser.Events exposing (onAnimationFrameDelta)
 import Canvas exposing (..)
 import Canvas.Settings exposing (..)
-import Canvas.Settings.Advanced exposing (..)
-import Canvas.Settings.Line exposing (..)
+import Canvas.Settings.Text exposing (TextAlign(..), TextBaseLine(..), align, baseLine, font)
 import Color exposing (Color)
-import Html exposing (Html, button, div, p)
-import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
+import Html exposing (Html)
 import Html.Events.Extra.Mouse as Mouse
 
 
@@ -19,363 +15,128 @@ main =
     Browser.element
         { init = init
         , update = update
-        , subscriptions = subscriptions
+        , subscriptions = always Sub.none
         , view = view
         }
 
 
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    onAnimationFrameDelta (always AnimationFrame)
-
-
+{-| TODO make this fullscreen-ish and dynamic on window resize
+-}
 h : number
 h =
-    500
+    900
 
 
 w : number
 w =
-    500
-
-
-type alias DrawingPointer =
-    { previousMidpoint : Point
-    , lastPoint : Point
-    }
+    900
 
 
 type alias Model =
-    { frames : Int
-    , pending : Array Renderable
-    , toDraw : List Renderable
-    , drawingPointer : Maybe DrawingPointer
-    , color : Color
-    , size : Int
+    { movingCircle : Maybe Point
+    , circle : Point
+    , permutations : List Permutation
+    , circleRadius : Float
     }
 
 
 type Msg
-    = AnimationFrame
-    | StartAt Point
+    = StartAt Point
     | MoveAt Point
     | EndAt Point
-    | SelectColor Color
-    | SelectSize Int
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { frames = 0
-      , pending = Array.empty
-      , toDraw = []
-      , drawingPointer = Nothing
-      , color = Color.lightBlue
-      , size = 20
+    ( { movingCircle = Nothing
+      , circle = ( 100, 100 )
+      , permutations = []
+      , circleRadius = 20
       }
     , Cmd.none
     )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg ({ frames, drawingPointer, pending, toDraw } as model) =
+update msg model =
     ( case msg of
-        AnimationFrame ->
-            incFrames model
-                |> flushPendingToDraw
-
         StartAt point ->
-            initialPoint point model
+            { model | movingCircle = getCircleAt point model }
 
-        MoveAt point ->
-            case drawingPointer of
-                Just pointer ->
-                    drawPoint point pointer model
-
-                Nothing ->
-                    model
-
-        EndAt point ->
-            case drawingPointer of
-                Just pointer ->
-                    finalPoint point pointer model
+        MoveAt ( _, y ) ->
+            case model.movingCircle of
+                Just ( x, _ ) ->
+                    { model | movingCircle = Just ( x, y ) }
 
                 Nothing ->
                     model
 
-        SelectColor color ->
-            selectColor color model
+        EndAt ( _, y ) ->
+            case model.movingCircle of
+                Just ( x, _ ) ->
+                    { model | movingCircle = Nothing, circle = ( x, roundToNearest100 y ) }
 
-        SelectSize size ->
-            selectSize size model
+                Nothing ->
+                    model
     , Cmd.none
     )
 
 
-incFrames : Model -> Model
-incFrames ({ frames } as model) =
-    { model | frames = frames + 1 }
-
-
-flushPendingToDraw : Model -> Model
-flushPendingToDraw ({ pending } as model) =
-    { model
-        | pending = Array.empty
-        , toDraw = Array.toList pending
-    }
-
-
-selectColor : Color -> Model -> Model
-selectColor color model =
-    { model | color = color }
-
-
-selectSize : Int -> Model -> Model
-selectSize size model =
-    { model | size = size }
-
-
-initialPoint : Point -> Model -> Model
-initialPoint p model =
-    { model
-        | drawingPointer = Just { previousMidpoint = p, lastPoint = p }
-    }
-
-
-drawPoint : Point -> DrawingPointer -> Model -> Model
-drawPoint newPoint { previousMidpoint, lastPoint } ({ pending } as model) =
-    let
-        newMidPoint =
-            controlPoint lastPoint newPoint
-    in
-    { model
-        | drawingPointer = Just { previousMidpoint = newMidPoint, lastPoint = newPoint }
-        , pending =
-            Array.push
-                (drawLine model
-                    [ path previousMidpoint [ quadraticCurveTo lastPoint newMidPoint ] ]
-                )
-                pending
-    }
-
-
-finalPoint : Point -> DrawingPointer -> Model -> Model
-finalPoint point { previousMidpoint, lastPoint } ({ pending } as model) =
-    { model
-        | drawingPointer = Nothing
-        , pending =
-            Array.push
-                (drawLine model
-                    [ path previousMidpoint [ quadraticCurveTo lastPoint point ] ]
-                )
-                pending
-    }
-
-
-controlPoint : Point -> Point -> Point
-controlPoint ( x1, y1 ) ( x2, y2 ) =
-    ( x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2 )
-
-
-drawLine : Model -> List Shape -> Renderable
-drawLine { color, size } line =
-    shapes
-        [ lineCap RoundCap
-        , lineJoin RoundJoin
-        , lineWidth (toFloat size)
-        , shadow { blur = 10, offset = ( 0, 0 ), color = getShadowColor color }
-        , stroke color
-        ]
-        line
-
-
-getShadowColor : Color -> Color
-getShadowColor color =
-    let
-        { red, green, blue } =
-            Color.toRgba color
-    in
-    Color.rgba red green blue 0.2
+roundToNearest100 : Float -> Float
+roundToNearest100 x =
+    100 * toFloat (round (x / 100))
 
 
 view : Model -> Html Msg
-view { color, size, toDraw } =
-    div []
-        [ p [ style "text-align" "center", style "font-size" "80%" ]
-            [ Html.text "Draw something! (mouse or touch)"
-            ]
-        , Canvas.toHtml ( w, h )
-            [ style "touch-action" "none"
-            , Mouse.onDown (.offsetPos >> StartAt)
+view { circle, movingCircle, circleRadius } =
+    Html.div []
+        [ Canvas.toHtml ( w, h )
+            [ Mouse.onDown (.offsetPos >> StartAt)
             , Mouse.onMove (.offsetPos >> MoveAt)
             , Mouse.onUp (.offsetPos >> EndAt)
             ]
-            toDraw
-        , div
-            [ style "max-width" (String.fromInt (w - 20) ++ "px")
-            , style "padding" "10px"
+            [ Canvas.clear ( 0, 0 ) w h
+            , Canvas.shapes
+                [ fill Color.white, stroke Color.black ]
+                [ Canvas.path (Maybe.withDefault circle movingCircle)
+                    [ Canvas.lineTo ( 200, 400 ), Canvas.lineTo ( 400, 200 ) ]
+                ]
+            , Canvas.shapes
+                [ fill Color.white, stroke Color.black ]
+                [ Canvas.circle (Maybe.withDefault circle movingCircle) circleRadius
+                , Canvas.circle ( 100, 200 ) circleRadius
+                , Canvas.circle ( 100, 300 ) circleRadius
+                , Canvas.circle ( 100, 400 ) circleRadius
+                ]
+            , textAt (Maybe.withDefault circle movingCircle) "1"
+            , textAt ( 100, 200 ) "2"
+            , textAt ( 100, 300 ) "3"
+            , textAt ( 100, 400 ) "4"
             ]
-            [ sizeControls color size
-            , colorButtons color
-            ]
+        , Html.div []
+            []
         ]
 
 
-sizeControls : Color -> Int -> Html Msg
-sizeControls selectedColor selectedSize =
-    let
-        brushes =
-            6
-
-        inc =
-            10
-
-        buttonSize =
-            brushes * inc
-
-        controls =
-            List.range 0 brushes
-                |> List.map
-                    (\i ->
-                        let
-                            size =
-                                max 2 (i * inc)
-                        in
-                        button
-                            [ style "-webkit-appearance" "none"
-                            , style "-moz-appearance" "none"
-                            , style "display" "block"
-                            , style "background-color" "transparent"
-                            , style "border" "none"
-                            , style "margin" "5px"
-                            , style "padding" "0"
-                            , style "min-width" (String.fromInt 30 ++ "px")
-                            , style "min-height" (String.fromInt buttonSize ++ "px")
-                            , style "outline" "none"
-                            , onClick (SelectSize size)
-                            ]
-                            [ div
-                                [ style "border-radius" "50%"
-                                , style "background-color" (Color.toCssString selectedColor)
-                                , style "border" ("3px solid " ++ (Color.white |> getShadowColor |> Color.toCssString))
-                                , style "width" (String.fromInt size ++ "px")
-                                , style "height" (String.fromInt size ++ "px")
-                                , style "margin" "0 auto"
-                                , style "box-shadow"
-                                    (if selectedSize == size then
-                                        "rgba(0, 0, 0, 0.4) 0px 4px 6px"
-
-                                     else
-                                        "none"
-                                    )
-                                , style "transition" "transform 0.2s linear"
-                                , style "transform"
-                                    (if selectedSize == size then
-                                        "translateY(-6px)"
-
-                                     else
-                                        "none"
-                                    )
-                                ]
-                                []
-                            ]
-                    )
-    in
-    div
-        [ style "display" "flex"
-        , style "flex-direction" "row"
-        , style "justify-content" "space-around"
-        , style "align-items" "center"
-        ]
-        controls
+textAt : Point -> String -> Renderable
+textAt =
+    Canvas.text
+        [ font { size = 16, family = "sans-serif" }, align Center, baseLine Middle ]
 
 
-colorButtons : Color -> Html Msg
-colorButtons selectedColor =
-    let
-        layout colors =
-            List.map (List.map (colorButton selectedColor) >> col) colors
-    in
-    div
-        [ style "display" "flex"
-        , style "flex-direction" "row"
-        , style "justify-content" "space-around"
-        ]
-    <|
-        layout
-            [ [ Color.lightRed
-              , Color.red
-              , Color.darkRed
-              ]
-            , [ Color.lightOrange
-              , Color.orange
-              , Color.darkOrange
-              ]
-            , [ Color.lightYellow
-              , Color.yellow
-              , Color.darkYellow
-              ]
-            , [ Color.lightGreen
-              , Color.green
-              , Color.darkGreen
-              ]
-            , [ Color.lightBlue
-              , Color.blue
-              , Color.darkBlue
-              ]
-            , [ Color.lightPurple
-              , Color.purple
-              , Color.darkPurple
-              ]
-            , [ Color.lightBrown
-              , Color.brown
-              , Color.darkBrown
-              ]
-            , [ Color.white
-              , Color.lightGrey
-              , Color.grey
-              ]
-            , [ Color.darkGrey
-              , Color.lightCharcoal
-              , Color.charcoal
-              ]
-            , [ Color.darkCharcoal
-              , Color.black
-              ]
-            ]
+pointDistance : Point -> Point -> Float
+pointDistance ( x1, y1 ) ( x2, y2 ) =
+    sqrt ((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 
 
-col : List (Html msg) -> Html msg
-col btns =
-    div [] btns
+getCircleAt : Point -> Model -> Maybe Point
+getCircleAt p { circle, circleRadius } =
+    if pointDistance circle p <= circleRadius then
+        Just circle
+
+    else
+        Nothing
 
 
-colorButton : Color -> Color -> Html Msg
-colorButton selectedColor color =
-    button
-        [ style "border-radius" "50%"
-        , style "background-color" (Color.toCssString color)
-        , style "display" "block"
-        , style "width" "40px"
-        , style "height" "40px"
-        , style "margin" "5px"
-        , style "border" "2px solid white"
-        , style "box-shadow"
-            (if selectedColor == color then
-                "rgba(0, 0, 0, 0.4) 0px 4px 6px"
-
-             else
-                "none"
-            )
-        , style "transition" "transform 0.2s linear"
-        , style "outline" "none"
-        , style "transform"
-            (if selectedColor == color then
-                "translateY(-6px)"
-
-             else
-                "none"
-            )
-        , onClick (SelectColor color)
-        ]
-        []
+type alias Permutation =
+    Array Int
